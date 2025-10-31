09180fe0a81b676a76f6a2903c65e8b9f263dde2
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-31 20:49:49 +0600

    tour gide sunthing update

--- src/pages/tourGide/TourGide.jsx
+++ src/pages/tourGide/TourGide.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Gide1 from './components/Gide1'
 
 const TourGide = () => {
   return (
-    <div className='text-center'>
-      <p className='text-7xl text-center py-32'>tour gide is coking</p>
+    <div className='text-center pt-2'>
+      <h1 className='text-4xl text-center  '>ট্রাভেল টিপস</h1>
+      <hr className='w-36 mx-auto my-5 border-2 border-green-600 '/>
+      <p className='text-center font-bold'>ভ্রমনের জন্য প্রয়োজনীয় জিনসি প্রত্র</p>
 
+       <Gide1 />
+
+       <p className='text-3xl mt-20'>The section is cooking....</p>
       <Link className='btn btn-group ' to={'/'}>Go to Home Page</Link>
     </div>
   )
--- src/pages/tourGide/components/Gide1.jsx
+++ src/pages/tourGide/components/Gide1.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import tourGideImage from '../../../assets/tour-gide/image1.png'
 
 const Gide1 = () => {
   return (
-    <div>
-        
+    <div className='mt-10 w-2/3 mx-auto'>
+        <img src={tourGideImage} alt="" className=''/>
     </div>
   )
 }
